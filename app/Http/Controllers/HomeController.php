12081d75c5f5227a52a7861e571479613f3b61ee
tusharjindal <tusharjindal1999@gmail.com>
2022-01-27 10:46:26 +0530

    added MVC changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -135,14 +135,16 @@ class HomeController extends Controller
             } 
             else if($role ==User::ROLE_TYPE_TEACHER ) 
             {
-                $teachers= Teachers::find($id);  
+                $find_teacher=new Teachers(); 
+                $teacher=$find_teacher->find($id); 
                 $find_user=new User(); 
                 $user=$find_user->find($id); 
                 return view('teachers.showprofile', compact('teachers'),compact('user')); 
             }
             else if($role ==User::ROLE_TYPE_STUDENT) 
             {
-                $students= Students::find($id);  
+                $find_student=new Students(); 
+                $students=$find_student->find($id); 
                 $find_user=new User(); 
                 $user=$find_user->find($id);  
                 return view('students.showprofile', compact('students'),compact('user')); 
